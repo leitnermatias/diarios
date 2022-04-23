8d1b945614e290ae6f8f89534fb39f3b32525057
Matias <leitnermatias22@gmail.com>
2022-04-23 17:33:18 -0300

    Changed get_html and validate_tags to functions

--- newspapers/newspaper.py
+++ newspapers/newspaper.py
@@ -3,6 +3,24 @@ from bs4 import BeautifulSoup
 import requests
 
 
+def get_html(url) -> str:
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.text
+
+    else:
+        raise Exception(f"[{response.status_code}] Couldnt GET html for {url} ")
+
+
+def validate_tags(*args):
+    for tag in args:
+        if tag is None:
+            return False
+
+    return True
+
+
 class Newspaper(ABC):
     def __init__(self, name: str):
         self.name = name
@@ -10,15 +28,6 @@ class Newspaper(ABC):
 
     def __repr__(self) -> str:
         return f"> {self.name}"
-
-    def get_html(self, url) -> str:
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            return response.text
-
-        else:
-            raise Exception(f"[{response.status_code}] Couldnt GET html for {url} ")
 
     def last_news(self):
         pass
